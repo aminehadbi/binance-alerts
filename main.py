5cfb5dc6f6e5c447c298495e617b963487fc1b61
Amine Hadbi <amine.hadbi@polymtl.ca>
2021-04-11 19:47:54 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   main.py 	modified:   your_file.txt
     Changes not staged for commit:
    	deleted:    core.py
    added multiprocessing in a weak way
    code is 4 time faster

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 import sys
-import multiprocessing
+from multiprocessing import Process
 from datetime import datetime
+import io
+from numba import guvectorize, njit
 
 def get_ticker_list():
     ticker_list = []
@@ -17,50 +19,44 @@ def get_candles(ticker, range, interval='1m'):
     url = "".join(('https://fapi.binance.com/fapi/v1/klines?symbol=',ticker,'&interval=',interval,'&limit=',str(range)))
     data = requests.get(url)
     candles = data.json()
-    return candles
-
-def calculate_rel_vol(candles):
+    volume_list = []
     total_vol = 0
     for i in candles:
-        total_vol += float(i[5])
-    current_ratio = round(100*float(candles[-1][5])/float(total_vol),2)
+        volume_list.append(float(i[5]))
+    total_vol = sum(volume_list)
     current_volume = float(candles[-1][5])
-    return current_ratio, current_volume
-
-def get_rel_vol(ticker, range, interval='1m'):
-    try:
-        candles = get_candles(ticker, range, interval)
-        current_ratio, current_volume = calculate_rel_vol(candles)
-        return current_ratio, current_volume
-    except Exception:
-        print(Exception)
-        return 1, 1
+    return total_vol, current_volume
 
 
 def scan_market(ticker, interval='1m', range='15'):
     results = {}
     #print(ticker, interval, range)
-    current_ratio, current_volume = get_rel_vol(ticker, range, interval)
+    total_vol, current_volume =  get_candles(ticker, range, interval)
+    current_ratio, current_volume = get_rel_vol(total_vol, current_volume, range, interval)
+
     if current_ratio > 10:
-            print('Current Ratio:', current_ratio, 'Current Volume:', current_volume, interval)
-    #        results[interval] = current_ratio
+            print('Current Ratio:', current_ratio, 'Current Volume:', current_volume, interval, ticker)
+            results[interval] = current_ratio
     return [current_ratio, current_volume, interval]
 
+@njit
+def calculate_ratios(total_vol, current_volume):
+    current_ratio = round(100*float(current_volume)/float(total_vol),2)
+    return current_ratio
 
-
-def main(value):
-    ticker_list = get_ticker_list()
-    print('Tickers list downloaded')
-    results_list = []
+def get_rel_vol(total_vol, current_volume, range=15, interval='1m'):
     try:
-        processes = []
-        p = multiprocessing.Process(target=scan_all_markets, args=(ticker_list,))
-        processes.append(p)
-        p.start()
-        for process in processes:
-            process.join()
-    except:
-        pass
+        current_ratio = calculate_ratios(total_vol, current_volume)
+        return current_ratio, current_volume
+
+    except Exception as E:
+        print(E)
+        return 1, 1
+
+
+
+
+
 
 
 def scan_all_markets(ticker_list):
@@ -70,13 +66,22 @@ def scan_all_markets(ticker_list):
         results = []
         #print('!!!', ticker)
         for i in filter_list:
-            a = scan_market(ticker, i, 15)
+            a =  scan_market(ticker, i, 15)
             results.append(a[0])
         ressum = sum(results)
         print(ticker, results, round(ressum))
         with open('your_file.txt', 'a') as f:
             f.write(str(ticker)+" "+str(results)+'\n')
 
+
+def main(value, ticker_list):
+  
+    results_list = []
+    try:
+         scan_all_markets(ticker_list)
+    except Exception as E:
+        print('Error!', E)
+        pass
 
 
 if __name__ == "__main__":
@@ -86,9 +91,29 @@ if __name__ == "__main__":
     with open('your_file.txt', 'a') as f:
         dt_object = datetime.fromtimestamp(timestamp)
         f.write(str(dt_object)+ '\n')
+    counter = 8
+    ticker_list = get_ticker_list()
+    length = len(ticker_list)//2
+    print('Tickers list downloaded')
+    #io.run(main('a', ticker_list))
+    ticker_list_rev = ticker_list.reverse()
+    print(ticker_list)
+    while counter < len(ticker_list):
+        p1 = Process(target=main, args=('a', ticker_list[counter-2:counter-1]))
+        p1.start()
+        p2 = Process(target=main, args=('a', ticker_list[counter-4:counter-3]))
+        p2.start()
+        
+        p3 = Process(target=main, args=('a', ticker_list[counter-6:counter-5]))
+        p3.start()
+        p4 = Process(target=main, args=('a', ticker_list[counter-8:counter-7]))
+        p4.start()        
+        p1.join()
+        p2.join()
+        p3.join()
+        p4.join()
 
-    main('a')
-
+        counter+=8
     now = datetime.now()
     timestamp = datetime.timestamp(now)
     with open('your_file.txt', 'a') as f:
