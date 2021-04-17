b0997337cccc8b45ab9669a82dbfcfc1dacaf0ab
Amine Hadbi <amine.hadbi@polymtl.ca>
2021-04-17 19:38:56 +0000

    cleaner

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
 import sys
-from multiprocessing import Process
+from multiprocessing import Queue, Process
 from datetime import datetime
 import io
 from numba import guvectorize, njit
+import pickle
+
+def get_current_price(ticker):
+    pass
+
 
 def get_ticker_list():
     ticker_list = []
@@ -11,12 +16,12 @@ def get_ticker_list():
     data = requests.get(url)
     candles = data.json()
     for i in candles['symbols']:
-        ticker_list.append(i['symbol'])
+        ticker_list.append(i['pair'])
     return ticker_list
 
 
 def get_candles(ticker, range, interval='1m'):
-    url = "".join(('https://fapi.binance.com/fapi/v1/klines?symbol=',ticker,'&interval=',interval,'&limit=',str(range)))
+    url = "".join(('https://fapi.binance.com/fapi/v1/continuousKlines?pair=',ticker,'&interval=',interval,'&limit=',str(range),'&contractType=PERPETUAL'))
     data = requests.get(url)
     candles = data.json()
     volume_list = []
@@ -27,17 +32,20 @@ def get_candles(ticker, range, interval='1m'):
     current_volume = float(candles[-1][5])
     return total_vol, current_volume
 
-
 def scan_market(ticker, interval='1m', range='15'):
-    results = {}
+    aboveRatio = {}
     #print(ticker, interval, range)
     total_vol, current_volume =  get_candles(ticker, range, interval)
     current_ratio, current_volume = get_rel_vol(total_vol, current_volume, range, interval)
 
     if current_ratio > 10:
             print('Current Ratio:', current_ratio, 'Current Volume:', current_volume, interval, ticker)
-            results[interval] = current_ratio
-    return [current_ratio, current_volume, interval]
+            aboveRatio = [ticker, interval, current_ratio]
+
+            with open('outfile.txt', 'a') as fp:
+                fp.write(str(aboveRatio)+'\n')
+                fp.close()
+    return [current_ratio, current_volume, interval], aboveRatio
 
 @njit
 def calculate_ratios(total_vol, current_volume):
@@ -66,31 +74,22 @@ def scan_all_markets(ticker_list):
         results = []
         #print('!!!', ticker)
         for i in filter_list:
-            a =  scan_market(ticker, i, 15)
+            a, aboveRatio =  scan_market(ticker, i, 15)
             results.append(a[0])
         ressum = sum(results)
         print(ticker, results, round(ressum))
-        with open('your_file.txt', 'a') as f:
-            f.write(str(ticker)+" "+str(results)+'\n')
-
+        return results, aboveRatio
 
 def main(value, ticker_list):
-  
-    results_list = []
-    try:
-         scan_all_markets(ticker_list)
-    except Exception as E:
-        print('Error!', E)
-        pass
+    results, aboveRatio = scan_all_markets(ticker_list)
+    return aboveRatio
 
-
-if __name__ == "__main__":
+def not_main():
     now = datetime.now()
-
     timestamp = datetime.timestamp(now)
-    with open('your_file.txt', 'a') as f:
-        dt_object = datetime.fromtimestamp(timestamp)
-        f.write(str(dt_object)+ '\n')
+    with open('outfile.txt', 'a') as fp:
+        fp.write(str(timestamp)+'\n')
+        fp.close()
     counter = 8
     ticker_list = get_ticker_list()
     length = len(ticker_list)//2
@@ -100,22 +99,20 @@ if __name__ == "__main__":
     print(ticker_list)
     while counter < len(ticker_list):
         p1 = Process(target=main, args=('a', ticker_list[counter-2:counter-1]))
-        p1.start()
         p2 = Process(target=main, args=('a', ticker_list[counter-4:counter-3]))
-        p2.start()
-        
         p3 = Process(target=main, args=('a', ticker_list[counter-6:counter-5]))
-        p3.start()
         p4 = Process(target=main, args=('a', ticker_list[counter-8:counter-7]))
-        p4.start()        
-        p1.join()
-        p2.join()
-        p3.join()
-        p4.join()
-
+        for p in [p1,p2,p3,p4]:
+            p.start()
+        for p in [p1,p2,p3,p4]:
+            p.join()
         counter+=8
     now = datetime.now()
     timestamp = datetime.timestamp(now)
-    with open('your_file.txt', 'a') as f:
-        dt_object = datetime.fromtimestamp(timestamp)
-        f.write(str(dt_object)+'\n')
+    with open('outfile.txt', 'a') as fp:
+        fp.write(str(timestamp)+'\n')
+        fp.close()
+
+if __name__ == "__main__":
+    while True:
+        not_main()
